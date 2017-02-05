435c4dba01a02faf6a3f2ab7e1c0c8e8d6196ae9
Audrey Fu <audreyfu14@gmail.com>
2017-02-04 20:07:29 -0800

    Add API Controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   resources :registered_applications, only: [:index, :new, :create, :show, :destroy]
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
 end
